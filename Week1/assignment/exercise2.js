6efb2d23d8becb8c79c0a2d4bd42c380e50498cb
Rama <120380006+rama-website@users.noreply.github.com>
2023-11-23 15:09:29 +0100

    editing assignment-w1

--- Week1/assignment/exercise2.js
+++ Week1/assignment/exercise2.js
@@ -1,39 +1,52 @@
+
 const mysql = require('mysql');
 
-// MySQL connection configuration
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'Rama-Mysql', 
-  password: 'rama-Mysql@', 
+  user: 'Rama-Mysql',
+  password: 'rama-Mysql@',
   database: 'world',
 });
 
-// Connect to the world database
 connection.connect((err) => {
   if (err) throw err;
 
   // Query 1: Countries with population greater than 8 million
-  connection.query(
-    'SELECT Name FROM country WHERE Population > 8000000',
-    (err, results) => {
-      if (err) throw err;
-      console.log('Countries with population greater than 8 million:');
-      console.log(results);
-    }
-  );
+  connection.query('SELECT Name FROM country WHERE Population > 8000000', (err, results) => {
+    if (err) throw err;
+    console.log('Countries with population greater than 8 million:');
+    results.forEach((row) => {
+      console.log(row.Name);
+    });
+  });
 
   // Query 2: Countries with "land" in their names
-  connection.query(
-    'SELECT Name FROM country WHERE Name LIKE "%land%"',
-    (err, results) => {
-      if (err) throw err;
-      console.log('Countries with "land" in their names:');
-      console.log(results);
-    }
-  );
+  connection.query('SELECT Name FROM country WHERE Name LIKE "%land%"', (err, results) => {
+    if (err) throw err;
+    console.log('Countries with "land" in their names:');
+    results.forEach((row) => {
+      console.log(row.Name);
+    });
+  });
 
-  // Add more queries for other questions following the same pattern
+  // Query 3: Other queries...
+  connection.query('SELECT Name FROM country WHERE Continent = "Europe"', (err, results) => {
+    if (err) throw err;
+    console.log('Countries in Europe:');
+    results.forEach((row) => {
+      console.log(row.Name);
+    });
+  });
 
-  // Close the MySQL connection
+  // Query 4: More queries...
+  connection.query('SELECT Name FROM country WHERE Population < 500000', (err, results) => {
+    if (err) throw err;
+    console.log('Countries with population less than 500,000:');
+    results.forEach((row) => {
+      console.log(row.Name);
+    });
+  });
+
   connection.end();
 });
+
